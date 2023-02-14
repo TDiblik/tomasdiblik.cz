e088e6512ea18e19505e27015499f1d7b23a8c09
Tomáš Diblík <dibla.tomas@post.cz>
2023-02-14 12:05:05 +0100

    [Feature] Sort projects by development timeframe

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", () => {
+  let all_titles = document.getElementsByClassName("projects-container-title");
+  let all_containers = document.getElementsByClassName("projects-container");
   let all_projects = document.getElementsByClassName("project-wrapper");
+
   document
     .getElementById("project-type-selector")
     .addEventListener("change", (e) => {
@@ -10,9 +13,26 @@ window.addEventListener("load", () => {
       for (let i = 0; i < all_projects.length; i++) {
         let project = all_projects[i];
         if (!project.classList.contains(class_to_contain)) {
-          project.classList.add("hidden");
+          project.classList.add("project-hidden");
         } else {
-          project.classList.remove("hidden");
+          project.classList.remove("project-hidden");
+        }
+      }
+
+      for (let i = 0; i < all_containers.length; i++) {
+        let current_title = all_titles[i];
+        let current_container = all_containers[i];
+
+        let visible_projects_count =
+          current_container.querySelectorAll(".project-wrapper").length;
+        let hidden_project_count = current_container.querySelectorAll(
+          ".project-wrapper.project-hidden"
+        ).length;
+
+        if (visible_projects_count === hidden_project_count) {
+          current_title.classList.add("project-hidden");
+        } else {
+          current_title.classList.remove("project-hidden");
         }
       }
     });
