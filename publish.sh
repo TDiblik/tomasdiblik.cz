f068392f2d1951bcee4dd0a2057f39d42192598a
Tomáš Diblík <dibla.tomas@post.cz>
2024-08-25 20:24:16 +0200

    [Feature] Modify publish.sh to be compatible with Linux and MacOs

--- publish.sh
+++ publish.sh
@@ -1,16 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # Dependencies needed
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y build-essential
-sudo apt install -y ruby ruby-dev
-sudo apt install -y libimage-exiftool-perl webp imagemagick
-sudo apt install -y imagemagick
-sudo apt install -y tidy
-sudo apt install -y ffmpeg
-
-sudo gem install bundler
+gem install bundler
 
 # Remove generated files
 rm -rf ./out
@@ -20,8 +11,7 @@ rm -rf ./src/.jekyll-cache/
 
 # Build site
 cd src/
-sudo bundle clean --force
-sudo bundle install
+bundle install
 JEKYLL_ENV=production bundle exec jekyll build
 cd ..
 
@@ -29,7 +19,7 @@ cd ..
 for d in $(find ./src/_site/pages/blog/ -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.webp" -o -name "*.jpeg" -o -name "*.ico" \)) ; do
     echo "$d"
     exiftool -all= $d 
-    convert -strip -interlace Plane -quality 80% $d $d
+    magick convert -strip -interlace Plane -quality 80% $d $d
 done
 rm -rf ./src/_site/pages/blog/**/**/*.jpg_original
 rm -rf ./src/_site/pages/blog/**/**/*.png_original
